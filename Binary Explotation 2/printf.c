d7df93ccf293eeed1ed6482b38f18654b34409e0
infosecucalgary <60866282+infosecucalgary@users.noreply.github.com>
2020-03-30 17:32:53 -0600

    Add files via upload

---
+++ Binary Explotation 2/printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char buffer[50];
+    printf("Hello, World!\n");
+
+    printf("Give me some good 'ol user input: ");
+    scanf("%s", buffer);
+
+    printf("%s\n", buffer);
+    printf(buffer);
+
+
+    printf("\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
